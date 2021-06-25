ccff3578e47a0d796c1ace77469e9beef5cefeb1
Nobinbd <nobin118960@gmail.com>
2021-06-25 07:15:50 +0600

    report added for Home owner and requirements updated

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns =[
     path('user/', views.userdash, name='userdash'),
     path('homeowner/', views.hodash, name='hodash'),
-   
+    path('pdfreport/', views.render_pdf_view, name='pdfreport'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,9 @@ from reservations.models import Reservation
 from houses.models import House
 from flats.models import Flat
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
 
 
@@ -46,3 +49,47 @@ def hodash(request):
 
     }
     return render(request, 'dashboard/hodash.html', context)
+
+
+@login_required()
+@user_passes_test(lambda u: u.groups.filter(name='Home_owner').exists())
+def render_pdf_view(request):
+    user = request.user
+    houses = House.objects.filter(owner=user)
+    totall_flats = []
+    total_reserved = []
+    for house in houses:
+        flats = Flat.objects.filter(house=house)
+        for flat in flats:
+            reserved = Reservation.objects.order_by('-id').filter(flat=flat)
+            total_reserved += reserved
+        totall_flats += flats           
+    flats_length = len(totall_flats)
+    available_flats = Flat.objects.filter(rent_status=True)
+    template_path = 'dashboard/report.html'
+    context = {
+        'user': user,
+        'houses':houses,
+        'houses_length':len(houses),
+        'flats':totall_flats,
+        'flats_length':flats_length,
+        'available_flats':available_flats,
+        'length_available_flats':len(available_flats),
+        'reserved':total_reserved,
+        'reserved_length':len(total_reserved), 
+    }
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    # response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    response['Content-Disposition'] = 'filename="report.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
